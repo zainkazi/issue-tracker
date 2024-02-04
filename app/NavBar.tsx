9e6c63b176d4f2b59971215596c2d67210af1ce8
Zain <zainkazi27@gmail.com>
2024-02-04 15:24:14 +0500

    added navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import { FaBug } from "react-icons/fa";
+
+const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex items-center px-4 py-4 space-x-6 border-b mb-6">
+      <Link href="/">
+        <FaBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.label}>
+            <Link
+              href={link.href}
+              className="text-zinc-600 hover:text-zinc-800 transition-colors"
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
