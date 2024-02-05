e820c291b551be9539749b15c47d3d68387ef3fc
Zain <zainkazi27@gmail.com>
2024-02-05 21:15:33 +0500

    fixed caching issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -35,6 +35,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       if (issue) await axios.patch("/api/issues/" + issue.id, data);
       else await axios.post("/api/issues", data);
       router.push("/issues");
+      router.refresh();
     } catch (error) {
       setError("An unexpected error occured");
       setSubmitting(false);
