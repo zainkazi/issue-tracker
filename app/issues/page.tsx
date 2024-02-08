a409ced4a9ae03acb934a7908c0ef1d0830ba080
Zain <zainkazi27@gmail.com>
2024-02-08 13:13:06 +0500

    filtered issues by status

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,9 +3,23 @@ import { Table } from "@radix-ui/themes";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import Link from "../components/Link";
 import IssueActions from "./IssueActions";
+import { Status } from "@prisma/client";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+interface searchParams {
+  searchParams: { status: Status };
+}
+
+const IssuesPage = async ({ searchParams }: searchParams) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: {
+      status,
+    },
+  });
 
   return (
     <div>
