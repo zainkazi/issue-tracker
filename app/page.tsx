f7ee9a75f3835924e91f276d8beaa7395bd13fd0
Zain <zainkazi27@gmail.com>
2024-02-08 14:24:49 +0500

    added pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      <Pagination itemCount={100} pageSize={10} currentPage={2} />
+    </div>
+  );
 }
